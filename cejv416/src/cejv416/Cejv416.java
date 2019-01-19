156ea54af7e91dfb899bab4ec07eab251774cd75
PIRUZ <PIRUZ@PIRUZ-PC>
2019-01-19 14:20:52 -0600

    calculation

--- cejv416/src/cejv416/Cejv416.java
+++ cejv416/src/cejv416/Cejv416.java
@@ -17,6 +17,7 @@ public class Cejv416 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello");
+        System.out.println(1+1);
     }
     
 }
